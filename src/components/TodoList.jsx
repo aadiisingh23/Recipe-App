20c944eaa3acc21f07d7d5cefd27ed996550a370
Aditya  Singh <aadityasinghh2360@gmail.com>
2025-12-30 23:21:22 +0530

    TodoList Done

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+const TodoList = () => {
+  let todos = [];
+  let editingId;
+  let editText;
+  let onToggle;
+  let onStartEdit;
+  let onSaveEdit;
+  let onCancelEdit;
+  let onDelete;
+  let onEditTextChange;
+  let onEditKeyPress;
+
+  return (
+    <>
+      <div className="space-y-2">
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            todo={todo}
+            index={index}
+            editingId={editingId}
+            editText={editText}
+            onToggle={onToggle}
+            onStartEdit={onStartEdit}
+            onSaveEdit={onSaveEdit}
+            onCancelEdit={onCancelEdit}
+            onDelete={onDelete}
+            onEditTextChange={onEditTextChange}
+            onEditKeyPress={onEditKeyPress}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default TodoList;
